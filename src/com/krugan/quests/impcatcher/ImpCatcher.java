15ad9e03ce573bee6543cb96b7a07f71c3671d00
teun <t.declercq@student.fontys.nl>
2020-09-30 09:19:22 +0200

    New ImpCatcher main class

---
+++ src/com/krugan/quests/impcatcher/ImpCatcher.java
@@ -0,0 +1,22 @@
+package com.krugan.quests.impcatcher;
+
+import com.krugan.quester.Main;
+import com.krugan.util.AdvancedTask;
+
+public class ImpCatcher extends AdvancedTask {
+    public ImpCatcher(Main main) {
+        super(main);
+    }
+
+    public class KillImpLootBeads extends AdvancedTask {
+        public KillImpLootBeads(Main main) {
+            super(main);
+        }
+    }
+
+    public class StartAndEndImpCatcher extends AdvancedTask {
+        public StartAndEndImpCatcher(Main main) {
+            super(main);
+        }
+    }
+}
